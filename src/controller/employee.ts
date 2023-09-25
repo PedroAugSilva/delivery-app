639272ebcc4de4633a5464857a869a90e90266b9
Guilherme <82233560+Guilherme12342br@users.noreply.github.com>
2023-09-25 08:24:59 -0300

    criação dos 'tipos' da tabela Employee em types

--- src/controller/employee.ts
+++ src/controller/employee.ts
@@ -1,3 +1,5 @@
+import { prisma } from "../lib/prisma";
+
 class Employee {
   async create() {}
 }
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -7,8 +7,25 @@ export type User = {
   password: string;
 };
 
+export type Employee = {
+  id: string;
+  name: string;
+  email: string;
+  key_acess: string;
+  role: string;
+};
+
+
+
 export type UserDTO = Omit<User, "id">;
 
 export interface UserJWT extends Omit<User, "password"> {
   token: string;
 }
+
+export type EmployeeDTO = Omit<Employee, "id">;
+
+export interface EmployeeJWT extends Omit<Employee, "key_acess"> {
+  token: string;
+}
+
